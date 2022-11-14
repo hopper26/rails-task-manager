ca684c7410d1aee5005b7ce791baad50884cc447
Hayley Lucas <halucas93@gmail.com>
2022-11-14 13:13:23 +1100

    solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
+# frozen_string_literal: true
+
+# app/routes.rb
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: "task_delete"
+
 end
